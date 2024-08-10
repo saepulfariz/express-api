cebc7b1345ebf7318b085903149d180c7c5ee95a
saepulfariz <saepulfariz108@gmail.com>
2024-08-11 06:14:16 +0700

    add cors list origin

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ const port = 3000
 
 require('dotenv').config()
 
+var CORS_LIST =
+    process.env.CORS_LIST;
+
+var whitelist = (CORS_LIST) ? CORS_LIST.split(';') : [];
+
 //import library CORS
 const cors = require('cors')
 
+var corsOptions = {
+    origin: function (origin, callback) {
+
+        if (origin === undefined) {
+            callback(null, true)
+        } else {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                // callback(new Error('Not allowed by CORS'))
+                callback('Not allowed by CORS')
+            }
+        }
+
+    }
+}
+
+
 //use cors
-app.use(cors())
+app.use(cors(corsOptions))
 
 //import body parser
 const bodyParser = require('body-parser')
@@ -24,6 +47,7 @@ app.use(bodyParser.json())
 
 //import route posts
 const postsRouter = require('./routes/posts');
+// app.use('/api/posts', cors(corsOptions), postsRouter); // use route posts di Express
 app.use('/api/posts', postsRouter); // use route posts di Express
 
 app.get('/', (req, res) => {
