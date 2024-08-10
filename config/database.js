fe2b6f0a05e737e312ba3d6e34a4eb3ef6929baa
saepulfariz <saepulfariz108@gmail.com>
2024-08-11 02:05:31 +0700

    add ENV configuration

--- config/database.js
+++ config/database.js
@@ -1,11 +1,26 @@
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_express_api'
-});
+const DB_HOST =
+    process.env.DB_HOST;
+const DB_USER =
+    process.env.DB_USER;
+const DB_PASSWORD =
+    process.env.DB_PASSWORD;
+const DB_NAME =
+    process.env.DB_NAME;
+const DB_PORT =
+    process.env.DB_PORT;
+const port = (DB_PORT) ? DB_PORT : 3306;
+
+const config = {
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: port
+}
+
+let connection = mysql.createConnection(config);
 
 connection.connect(function (error) {
     if (!!error) {
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+
 //import library CORS
 const cors = require('cors')
 
